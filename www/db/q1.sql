c348eb96024188d8675c86174e2685c460be61f5
Paulo Matias <paulo.matias@usp.br>
2016-03-06 23:16:21 -0300

    Remove leaked files

--- www/db/q1.sql
+++
@@ -1,4 +0,0 @@
-WITH RECURSIVE
-loop(s,i,accum) AS (VALUES((select group_concat(nome,'') from (select nome from procedimentos limit 5000)), 1, 0) UNION ALL
-	 SELECT s, i+1, (accum+unicode(substr(s,i,1)))%256 FROM loop WHERE i<=71905)
-SELECT accum FROM loop WHERE i=71906;
